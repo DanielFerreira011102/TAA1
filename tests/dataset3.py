fc489879e179b20d01e441f1481c8dbadc78d68f
leina <djbf@ua.pt>
2023-04-14 22:51:42 +0100

    point biserial correlation to target

--- tests/dataset3.py
+++ tests/dataset3.py
@@ -1,7 +1,8 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import skew
+from scipy.stats import pointbiserialr
+
 from leina.preprocessing import split_data, label_encode, ordinal_encode
 
 sns.set_theme(style="darkgrid")
@@ -11,6 +12,7 @@ df = pd.read_csv('../bank.csv')
 
 numeric_col = df.select_dtypes(include=['int64', 'float64']).columns
 category_col = df.select_dtypes(include=['object']).columns
+deposit_col = df[['deposit']]
 
 columns = df.columns
 X, y = split_data(df)
@@ -20,6 +22,42 @@ y = label_encode(y)
 X = pd.DataFrame(X, columns=df.columns[:-1])
 y = pd.DataFrame(y, columns=['deposit'])
 df_encoded = pd.concat([X, y], axis=1)
+
+# calculate the point-biserial correlation coefficients
+correlations = []
+for col in X.columns:
+    corr, pval = pointbiserialr(X[col], y['deposit'])
+    correlations.append(corr)
+
+# create a bar plot of the correlations using seaborn
+# create a custom color palette
+cmap = sns.dark_palette('#69d', n_colors=len(X.columns), reverse=True)
+cor_range = max(correlations) - min(correlations)
+color_mapping = dict(zip(sorted(correlations), cmap))  # map correlations to colors
+
+sns.set_style('darkgrid')
+plt.figure(figsize=(12, 8))
+ax = sns.barplot(x=X.columns, y=correlations, palette=cmap)
+
+# set the color of each bar based on its correlation value
+for i, corr in enumerate(correlations):
+    color = color_mapping[corr]
+    ax.get_children()[i].set_color(color)
+
+plt.title('Point-Biserial Correlation of every feature to desired target', fontsize=16)
+plt.xlabel('Features', fontsize=16)
+plt.ylabel('Correlation Coefficient', fontsize=16)
+
+# set space and rotation for x-axis labels
+ax.set_yticklabels(ax.get_yticklabels(), fontsize=16)
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right', fontsize=16)
+plt.subplots_adjust(bottom=.2)
+
+plt.tight_layout()
+plt.show()
+
+
+raise Exception
 
 plt.figure()
 # Visualize distribution of dataset information
